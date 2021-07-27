a8f99ae4b8e415e51d80f784312b8cfec443923e
Lucas Vidal <lcv55@hotmail.com>
2021-07-27 18:21:46 -0300

    Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
+
+ENV['RAILS_ENV'] = 'test'
+
+require_relative '../spec/dummy/config/environment'
+ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
