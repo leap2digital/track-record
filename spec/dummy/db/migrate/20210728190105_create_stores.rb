13c11d9cc87290f35811e7c57ac9efb3a660b1d6
Lucas Vidal <lcv55@hotmail.com>
2021-07-28 16:02:17 -0300

    Create store model

---
+++ spec/dummy/db/migrate/20210728190105_create_stores.rb
@@ -0,0 +1,9 @@
+class CreateStores < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stores do |t|
+      t.belongs_to :owner, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
