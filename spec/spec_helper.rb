a7baea81f95b6c93967c420e49e10f94b9ada787
Lucas Vidal <lcv55@hotmail.com>
2021-07-28 15:41:41 -0300

    Update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,6 @@ ENV['RAILS_ENV'] = 'test'
 
 require_relative '../spec/dummy/config/environment'
 ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
+
+require 'track_record'
+require 'rspec/rails'
