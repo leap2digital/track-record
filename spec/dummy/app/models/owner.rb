1fa8bf0d80ab186aa87a96f0378d38a914205049
Lucas Vidal <lcv55@hotmail.com>
2021-07-28 16:19:24 -0300

    Add TrackRecord to models

--- spec/dummy/app/models/owner.rb
+++ spec/dummy/app/models/owner.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Owner < ApplicationRecord
+  include TrackRecord
+
   has_many :stores
 end
--- spec/dummy/app/models/store.rb
+++ spec/dummy/app/models/store.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Store < ApplicationRecord
+  include TrackRecord
+
   belongs_to :owner
 end
