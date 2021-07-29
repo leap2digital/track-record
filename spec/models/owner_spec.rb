70fc9d2eb6e4c1132cb59cebb209519f963806e3
Lucas Vidal <lcv55@hotmail.com>
2021-07-29 19:50:49 -0300

    Update owner spec

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -6,15 +6,27 @@ RSpec.describe Owner, type: :model do
   before(:all) do
     @owner = described_class.create(name: 'test_owner')
   end
-
-  after(:all) do
-    described_class.all.each do |e|
-      e.destroy
-    end
-  end
   
   it 'has a name' do
-    owner = described_class.first
+    owner = described_class.last
     expect(owner.name).to eq(@owner.name)
+  end
+
+  it 'has a store' do
+    @owner.stores.create(name: 'test_store')
+    expect(@owner.stores.last).to_not be_nil
+  end
+
+  it 'records creation in elasticsearch' do
+    Elasticsearch::Model.client.indices.refresh(index: @owner.audit_index_name)
+    expect(@owner.feed['Owner'].last['event_action']).to eq('create')
+    expect(@owner.feed['Owner'].last['changes']['name']).to eq([nil, 'test_owner'])
+  end
+
+  it 'records update in elasticsearch' do
+    @owner.update(name: 'new_test_owner')
+    Elasticsearch::Model.client.indices.refresh(index: @owner.audit_index_name)
+    expect(@owner.feed['Owner'].last['event_action']).to eq('update')
+    expect(@owner.feed['Owner'].last['changes']['name']).to eq(['test_owner', 'new_test_owner'])
   end
 end
